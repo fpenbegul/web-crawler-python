a5289cb4b2dd096540fbc7dbf90fc4af8ae9e496
Duan Nunes Alves da Silva <39650422+duanpsycho@users.noreply.github.com>
2018-05-27 15:41:32 -0300

    Update WebCrawler.py

--- WebCrawler.py
+++ WebCrawler.py
@@ -22,21 +22,20 @@ def abrirConexaoCloud():
         status = 'Ok'
     except:
         print("Nao foi possivel conectar ao IBM Cloud")
+        conn = []
+        cur = []
         status = 'nOk'
     return conn, cur, status
 
-#Caso o processamento seja efetuado com Sucesso, comita ao final da execução
-def fechaConexao(resultado):
-    if resultado == 'S':
+#Fecha conexao se a conexao foi aberta com status ok
+def fechaConexao(status):
+    if status == 'Ok':
        conn.commit()
-    else:
-       conn.rollback()
-    cur.close()
-    conn.close()
+       conn.close()
+       cur.close()
 
-    #Verifica existencia da tabela, se negativo, cria, se positivo, dropa e cria novamente
+#Verifica existencia da tabela, se negativo, cria, se positivo, dropa e cria novamente
 def criaBase(nome_tabela):
-    #Verifica se a tabela já existe
     cur.execute("SELECT 1 FROM information_schema.tables where table_name = lower('{0}')".format(nome_tabela))
     x = cur.fetchone()
     if 1 in x:
@@ -45,10 +44,11 @@ def criaBase(nome_tabela):
        cur.execute("CREATE TABLE {0} (link text)".format(nome_tabela))
     conn.commit()
 
-    #Insere informações no DB
+#Insere informações no DB
 def insere(dados, nome_tabela):
     cur.execute("INSERT INTO {0} (link) VALUES ('{1}')".format(nome_tabela, dados))
 
+#Consulta DB para retornar os links inseridos
 def leituraDados(tabela):
       cur.execute("SELECT * FROM {0}".format(tabela))
       y = []
@@ -56,7 +56,7 @@ def leituraDados(tabela):
           y.append(x[0])
       return y
 
-#Abre a URL informada, recebe uma lista de Links
+#Abre a URL informada, recebe uma lista de Links e o nome da tabela
 def webCrawler(url, lista_links, nome_tabela):
     try:
         pagina = urllib.request.urlopen(url)
@@ -67,6 +67,7 @@ def webCrawler(url, lista_links, nome_tabela):
             next_link = x.get('href')
             if next_link not in lista_links:
                 lista_links.append(next_link)
+                print(next_link)
                 insere(next_link, nome_tabela)
                 lista_links.append(webCrawler(next_link, lista_links, nome_tabela))
     except:
@@ -79,7 +80,7 @@ saida = []
 tabela = "guardaLinks"
 
 #URL de verificação
-url = input("Digite a página web: ")
+url = input("Ex: http://www.google.com.br \n Digite a página web: \n ")
 
 #String de Conexão
 conn, cur, status = abrirConexaoCloud()
@@ -88,24 +89,21 @@ conn, cur, status = abrirConexaoCloud()
 if status == 'Ok':
     #Verifica se a tabela existe
     criaBase(tabela)
-    
+
     #Inicia o Crawler
     webCrawler(url, saida, tabela)
-    
+
     #Retorna os dados
     links = leituraDados(tabela)
-    
+
     #Valida retorno do select
     if len(links) == 0:
         print("Nao foi encontrado nenhum link na pagina informada")
     else:
         for x in range(len(links)):
             print(x, " - ", links[x])
-     #seta resultado para commit       
-    resultado = "S"
-else:
-    #Realiza Rollback
-    resultado = 'N'
-#Fecha Conexao    
-fechaConexao(resultado)
+    # Fecha Conexao
+    fechaConexao(status)
+#fim do If
+
 print("Fim")
