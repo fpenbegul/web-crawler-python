54ec70a2f3f9e0aa0d725fd571100b3ab2355f9c
Duan Nunes Alves da Silva <39650422+duanpsycho@users.noreply.github.com>
2018-05-26 23:21:09 -0300

    WebCrawler.py
    Script in Python for WebCrawler.

---
+++ WebCrawler.py
@@ -0,0 +1,96 @@
+#HTML Parser
+from bs4 import BeautifulSoup
+#Request para HTML
+import urllib.request
+#Conexoes ao Cloud
+import psycopg2
+import ssl
+#Recompile
+import re
+
+#Metodos
+#Abre a conexão IBM Cloud
+    def abrirConexaoCloud(self):
+        try:
+            conn = psycopg2.connect(host='HOST',
+                                    port = 'PORTA',
+                                    user = 'USUARIO',
+                                    password = 'SENHA',
+                                    database = 'BASE')
+        except:
+            print("Nao foi possivel conectar.")
+        cur = conn.cursor()
+        return conn, cur
+
+    #Caso o processamento seja efetuado com Sucesso, comita ao final da execução
+    def fechaConexao(resultado):
+        if resultado == 'S':
+            conn.commit()
+        else:
+            conn.rollback()
+        cur.close()
+        conn.close()
+
+    #Verifica existencia da tabela, se negativo, cria, se positivo, dropa e cria novamente
+    def criaBase(nome_tabela):
+        #Verifica se a tabela já existe
+        cur.execute("SELECT 1 FROM information_schema.tables where table_name = lower('{0}')".format(nome_tabela))
+        x = cur.fetchone()
+        if 1 in x:
+            cur.execute("TRUNCATE TABLE {0}".format(nome_tabela))
+        else:
+            cur.execute("CREATE TABLE " + nome_tabela + " (link text)")
+        conn.commit()
+
+    #Insere informações no DB
+    def insere(dados):
+        cur.execute("INSERT INTO guardaLinks (link) VALUES ('{0}')".format(dados))
+    
+    #Retorna dados inseridos no DB
+    def leituraDados(tabela):
+        cur.execute("SELECT * FROM {0}".format(tabela))
+        y = []
+        for x in cur:
+            y.append(x[0])
+        return y
+
+#Abre a URL informada, recebe uma lista de Links
+def webCrawler(url, lista_links):
+    pagina = urllib.request.urlopen(url)
+    #Realiza o parser no HTML da pagina solicitada
+    BSoup = BeautifulSoup(pagina, "html.parser", from_encoding="iso-8859-1")
+    #Varre o HTML da pagina para identificar outros links existentes
+    for x in BSoup.findAll('a', attrs={'href': re.compile("^http://")}):
+        next_link = x.get('href')
+        if next_link not in lista_links:
+            lista_links.append(next_link)
+            insere(next_link)
+            lista_links.append(webCrawler(next_link, lista_links))
+
+######## Inicio ########
+
+#Inicializa variaveis
+lista = []
+saida = []
+tabela = "guardaLinks"
+
+#URL de verificação
+url = input("Digite a página web: ")
+
+#String de Conexão
+conn, cur = abrirConexaoCloud()
+
+#Verifica se a tabela existe
+criaBase(tabela)
+
+#Inicia o Crawler
+webCrawler(url, saida)
+
+#Retorna os dados
+links = leituraDados(tabela)
+for x in range(len(links)):
+    print(x, " - ", links[x])
+
+resultado = "S"
+fechaConexao(resultado)
+print("Fim")
